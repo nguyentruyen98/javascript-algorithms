43adacaf660db3d9879e9deec25098075d87c5f7
nguyentruyen98 <haohaothieugia@gmail.com>
2023-05-05 21:44:02 +0700

    [#11] recursion/leetCode/mergeTwoLists

---
+++ Recursion/LeetCode/MergeTwoSortedList.js
@@ -0,0 +1,25 @@
+//* Ref: https://leetcode.com/problems/merge-two-sorted-lists/
+
+class ListNode {
+  constructor(val, next) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+export function mergeTwoLists(l1, l2) {
+  if (!l1) return l2;
+  else if (!l2) return l1;
+  else if (l1.val <= l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists(l1, l2.next);
+    return l2;
+  }
+}
+
+const list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+const list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+const a = mergeTwoLists(list1, list2);
+console.log(a);

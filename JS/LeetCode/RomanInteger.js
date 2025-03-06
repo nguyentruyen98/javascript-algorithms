4d3c83989a8d6dee087b84537a0a091ce285896b
nguyentruyen98 <haohaothieugia@gmail.com>
2025-03-06 23:56:57 +0700

    Add Roman to Integer conversion function

---
+++ JS/LeetCode/RomanInteger.js
@@ -0,0 +1,32 @@
+// ** https://leetcode.com/problems/roman-to-integer/description/
+const CONVERT = {
+  I: 1,
+  IV: 4,
+  IX: 9,
+  V: 5,
+  X: 10,
+  XL: 40,
+  XC: 90,
+  L: 50,
+  C: 100,
+  CD: 400,
+  CM: 900,
+  D: 500,
+  M: 1000,
+};
+
+const romanInteger = (s) => {
+  let result = 0;
+  let i = 0;
+  while (i < s.length) {
+    const char = s.slice(i, i + 2);
+    if (CONVERT[char]) {
+      result += CONVERT[char];
+      i += 2;
+    } else {
+      result += CONVERT[s[i]];
+      i += 1;
+    }
+  }
+  return result;
+};
